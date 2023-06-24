0a112c22354ad618163ecab3884f2a7c61a1b6ee
MehraSaurav <133487907+MehraSaurav@users.noreply.github.com>
2023-06-25 01:57:14 +0530

    Update youtube_link_from_rekhta.py

--- ghazals/youtube_link_from_rekhta.py
+++ ghazals/youtube_link_from_rekhta.py
@@ -2,26 +2,19 @@ from Get_Ghazals import get_ghazal
 import requests
 from bs4 import BeautifulSoup
 
-def get_youtube_link(ghazal_link):
+def youtube_link_from_rekhta(ghazal_link):
     req = requests.get(ghazal_link)
     soup = BeautifulSoup(req.content, "html.parser")
-    tag = soup.find('div', class_="videoListItem clearfix")
-    try:
-        link = tag.find('img').get("src")
-        link = link.replace("vi/", "watch?v=")
-        link = link.replace("img.", "")
-        link = link[::-1]
-        i = link.index("/")
-        link = link[i + 1:]
-        link = link[::-1]
-        return link
-    except:
-        return ''
+    tags = soup.find_all('div', class_="videoListItem clearfix")
+    youtube_links = {}
+    for tag in tags:
+        youtube_links[f"https://www.youtube.com/watch?v={tag['data-id']}"] = tag['data-desc']
+    return youtube_links
 
 if __name__ == "__main__":
     poet_name = input()
     ghazals = get_ghazal(poet_name)
     for ghazal in ghazals:
-        ghazals[ghazal]["Youtube Links"] = [get_youtube_link(ghazals[ghazal]["Rekhta Link"])]
+        ghazals[ghazal]["Rekhta Youtube Links"] = youtube_link_from_rekhta(ghazals[ghazal]["Rekhta Link"])
         print(ghazal)
         print(ghazals[ghazal])
